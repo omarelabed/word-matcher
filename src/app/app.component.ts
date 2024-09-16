e2826d49eeee687d5659b2165e261002aeceae9e
Omar Elabed <elabedomar@gmail.com>
2024-09-16 18:21:28 +0200

    add material ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MatTabGroup, MatTab } from '@angular/material/tabs';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, MatTabGroup, MatTab],
+  template: `
+  <main>
+  <h1>Word Matcher</h1>
+  <mat-tab-group>
+    <mat-tab label="Erfassen">
+      Erfassen / Liste
+    </mat-tab>
+    <mat-tab label="Trainieren">
+      Trainieren
+    </mat-tab>
+    <mat-tab label="Prüfung">
+      Prüfung
+    </mat-tab>
+  </mat-tab-group>
+</main>
+`,
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'word-matcher';
